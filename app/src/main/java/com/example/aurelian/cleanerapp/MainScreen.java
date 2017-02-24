3ba35ba014ca87fc96eb6669c757c6b39a2a3223
Aurelian <bugaauradi@yahoo.com>
2017-02-24 23:40:04 +0200

    #4 Memory Arc draw method added

--- app/src/main/java/com/example/aurelian/cleanerapp/MainScreen.java
+++ app/src/main/java/com/example/aurelian/cleanerapp/MainScreen.java
@@ -1,13 +1,19 @@
 package com.example.aurelian.cleanerapp;
 
+import android.content.Context;
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
@@ -58,5 +64,40 @@ public class MainScreen extends AppCompatActivity {
         // binding with ViewPager
         bind.bnve.setupWithViewPager(bind.vp);*/
 
+        ImageView memoryArc = (ImageView) findViewById(R.id.MemoryArc);
+        double percente = 100.0;
+        memoryArc.setImageBitmap(getMemoryArcBtm(this , percente));
+
+    }
+
+    private Bitmap getMemoryArcBtm(Context context, double percentage) {
+
+        int width = 400;
+        int height = 400;
+        int stroke = 30;
+        int padding = 5;
+        float density = context.getResources().getDisplayMetrics().density;
+
+        //Paint for arc stroke.
+        Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG | Paint.DITHER_FLAG | Paint.ANTI_ALIAS_FLAG);
+        paint.setStrokeWidth(stroke);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+
+        final RectF arc = new RectF();
+        arc.set((stroke/2) + padding, (stroke/2) + padding, width-padding-(stroke/2), height-padding-(stroke/2));
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        //First draw full arc as background.
+        paint.setColor(Color.argb(75, 255, 255, 255));
+
+        double endAngle = (272.0 / 100.0 * percentage);
+        canvas.drawArc(arc, 135, 272, false, paint);
+        //Then draw arc progress with actual value.
+        paint.setColor(Color.WHITE);
+        canvas.drawArc(arc, 135, (float)endAngle, false, paint);
+
+        return  bitmap;
     }
 }
